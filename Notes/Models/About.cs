8577a6f5ae098db0ead6e2f9291c4ce4cff7c6c5
mateop11 <122656503+mateop11@users.noreply.github.com>
2024-12-01 11:34:30 -0500

    Agregar archivos de proyecto.

---
+++ Notes/Models/About.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notes.Models
+{
+    internal class About
+    {
+        public string Title => AppInfo.Name;
+
+        public string Nombre => "Mateo Pillajo Guijarro";
+        public string Version => AppInfo.VersionString;
+        public string MoreInfoUrl => "https://github.com/mateop11";
+        public string Message => "Mi nombre es Mateo, tengo 22 años y estudio Ingenieria de Software, practco box y me apasiona mucho el fútbol.";
+    }
+}
