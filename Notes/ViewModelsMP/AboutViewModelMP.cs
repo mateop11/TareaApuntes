eb47895861d2b1f074f3bd98f6e96f0337b06093
mateop11 <122656503+mateop11@users.noreply.github.com>
2024-12-22 11:33:10 -0500

    Se creo  el modelo de vista Nota, y se limpio el código subyacente de la nota

--- Notes/ViewModelsMP/AboutViewModelMP.cs
+++ Notes/ViewModelsMP/AboutViewModelMP.cs
@@ -1,12 +1,27 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.Input;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Notes.ViewModelsMP
 {
     internal class AboutViewModelMP
     {
+        public string Title => AppInfo.Name;
+        public string Version => AppInfo.VersionString;
+        public string MoreInfoUrl => "https://aka.ms/maui";
+        public string Message => "This app is written in XAML and C# with .NET MAUI.";
+        public ICommand ShowMoreInfoCommand { get; }
+
+        public AboutViewModelMP()
+        {
+            ShowMoreInfoCommand = new AsyncRelayCommand(ShowMoreInfo);
+        }
+
+        async Task ShowMoreInfo() =>
+            await Launcher.Default.OpenAsync(MoreInfoUrl);
     }
 }
